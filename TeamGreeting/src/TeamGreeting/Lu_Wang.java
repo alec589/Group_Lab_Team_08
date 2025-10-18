23c39475a43cda9bf42d396337903b80fc206286
LuWang627 <wang.l6@northeastern.edu>
2025-10-18 18:35:02 -0400

    hello from lu wang

---
+++ TeamGreeting/src/TeamGreeting/Lu_Wang.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TeamGreeting;
+
+/**
+ *
+ * @author luwang
+ */
+public class Lu_Wang {
+    public static void main(String[] args) {
+        System.out.println("Hello from Lu Wang!");
+    }
+
+}
